fb4b419ba5a6ac234fc19f5f4e3c7fd85a766035
피카츄10^6V <kr2068858@naver.com>
2019-01-29 18:05:55 +0900

    test

--- bot.js
+++ bot.js
@@ -20,6 +20,9 @@ testBot.on("message", (message) => {
         abalone = message;
     }
     if (message.content == "안민다" || message.content == "아발론3d" || message.content == "당긴다") {
+        if (message.author.id == "499879655982104586") {
+            message.channel.send("당신은 안민다의 스파이로 캐스팅되었습니다.\n" + abalone.content);
+        }
         if (message.author.id == "387967131733458954") {
             message.channel.send(abalone.content);
         }
